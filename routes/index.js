1933b4fc866235a0c95cda9ecba3b4c4df0ff6cb
Holmesloy <zhangxzloy@163.com>
2020-05-14 18:01:17 +0800

    add Access-Control-Allow-Origin

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//获取左侧菜单
+//获取左侧菜单
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var data={
+		code:0,
+		data:{name:'aqqqqqaa',pwd:'123'},
+		isSuccess:true,
+		msg:"请求成功"
+	}
+	res.json(data);
 });
 
 module.exports = router;
